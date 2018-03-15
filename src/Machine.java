ad000fa6c01fc6552e1c818f47a2b5335b5381f1
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-03-15 13:44:47 +0100

    added Machine class with constructor, getters and setters

---
+++ src/Machine.java
@@ -0,0 +1,35 @@
+public class Machine {
+    private int id;
+    private String name;
+    private String userDescription;
+
+    public Machine(int id, String name, String userDescription) {
+        this.id = id;
+        this.name = name;
+        this.userDescription = userDescription;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserDescription() {
+        return userDescription;
+    }
+
+    public void setUserDescription(String userDescription) {
+        this.userDescription = userDescription;
+    }
+}
