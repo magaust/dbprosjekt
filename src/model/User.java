b778cf1032aba3ea10fc21b3e3d20c34ad84f8ee
Øivind Albrigtsen <formalatist@gmail.com>
2018-03-20 14:28:50 +0100

    removed workouts from user and added override for toString

--- src/model/User.java
+++ src/model/User.java
@@ -5,16 +5,10 @@ import java.util.ArrayList;
 public class User {
     private int id;
     private String name;
-    private ArrayList<Workout> workouts;
 
     public User(int id, String name) {
         this.id = id;
         this.name = name;
-        workouts = new ArrayList<>();
-    }
-
-    public void addWorkout(Workout workout) {
-        workouts.add(workout);
     }
 
     public String getName() {
