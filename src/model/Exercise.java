a56120e99eb2638fbd6f808af06ebec9f6c6236c
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-03-22 14:34:29 +0100

    added exerciseDBManager

--- src/model/Exercise.java
+++ src/model/Exercise.java
@@ -5,11 +5,13 @@ public class Exercise {
 	private int id;
     private String name;
     private String description;
+    private boolean isMachine;
 	
-    public Exercise(int id, String name, String description) {
+    public Exercise(int id, String name, String description, boolean machine) {
     		this.id = id;
     		this.name = name;
     		this.description = description;
+    		this.isMachine = machine;
     }
     
     public int getId() {
@@ -23,5 +25,6 @@ public class Exercise {
     public String getDescription() {
     		return description;
     }
-    
+
+    public boolean getIsMachine() { return isMachine; }
 }
