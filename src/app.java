f3c729bd6f845df19a003b6471014682f2b6aac1
Øivind Albrigtsen <formalatist@gmail.com>
2018-03-22 12:09:43 +0100

    remove .class files and added create and get workout in WorkoutDBManager

--- src/app.java
+++ src/app.java
@@ -1,6 +1,9 @@
 import model.*;
 import database.*;
 
+import java.util.Date;
+
+
 class app {
 
 	public static void main(String[] args){
@@ -17,6 +20,12 @@ class app {
 			//get that same user back from the db
 			User userFetchedFromDB = userDBManager.getUser(userID);
 			System.out.println(userFetchedFromDB);
+
+			//create a workout
+			int workoutID = (int)(Math.random()*100000);
+			Workout workout = new Workout(workoutID, 74517, 10.0, 1, 1, "Test workout", new java.sql.Date(new Date().getTime()));
+			WorkoutDBManager workoutDBManager = new WorkoutDBManager();
+			workoutDBManager.createWorkout(workout);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
