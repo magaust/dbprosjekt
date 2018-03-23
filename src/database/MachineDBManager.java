020c656c621b495bfbdccf64ab7765ce7499ac39
Øivind Albrigtsen <formalatist@gmail.com>
2018-03-23 14:00:23 +0100

    asd

--- src/database/MachineDBManager.java
+++ src/database/MachineDBManager.java
@@ -1,6 +1,10 @@
 package database;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Machine;
 
@@ -22,5 +26,22 @@ public class MachineDBManager extends DBManager{
 		statement.getStatement().executeUpdate();
 	}
 
-	
+	public List<Machine> getAllMachines() throws Exception{
+		String query = "select ApparatID, navn, brukerBeskrivelse from apparat";
+
+		Statement stmnt = connection.createStatement();
+		ResultSet results = stmnt.executeQuery(query);
+
+		ArrayList<Machine> machines = new ArrayList<>();
+
+		while(results.next()) {
+			machines.add(new Machine(
+					results.getInt("ApparatID"),
+					results.getString("navn"),
+					results.getString("BrukerBeskrivelse")
+			));
+		}
+
+		return machines;
+	}
 }
